cf88bdf89f3056870be1d42d521b9e8db4c04322
Davide Luppoli <davide@davideluppoli.it>
2024-06-05 10:01:15 +0200

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ErastoteneComponent } from './components/erastotene/erastotene.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', component:HomeComponent },
+    { path:'erastotene', component:ErastoteneComponent },
+    { path:'**', component:NotfoundComponent }
+];
